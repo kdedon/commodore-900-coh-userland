39b37243c6628ed6edb791b4e6a667cc843f8d36
kdedon <kdedon@gmail.com>
2026-09-12 17:11:39 -0500

    Settle four ownership questions the tree stated two answers to: the hr driver is Pleban's, knapsack is the donor's under the 2015 release, and compress gets a regime of its own naming the authors its own comment names. The INETCO strings in head and ps sit inside the Mark Williams source the release reaches.

--- hr/drv/hr.c
+++ hr/drv/hr.c
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2026 Michal Pleban.
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
 
 
 /*
--- hr/drv/hr.h
+++ hr/drv/hr.h
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2026 Michal Pleban.
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
 
 
 #define	NARG	4			/* # words in a message */
